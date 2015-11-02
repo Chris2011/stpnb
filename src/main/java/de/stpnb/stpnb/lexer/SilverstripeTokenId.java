df9891becbbe0961c73c9e80dc826d508fd35fda
Juri Demeschew <demeschew@gmail.com>
2015-11-02 06:01:19 +0100

    Autogenerate files for silverstripe netbeans plugin

---
+++ src/main/java/de/stpnb/stpnb/lexer/SilverstripeTokenId.java
@@ -0,0 +1,22 @@
+package de.stpnb.stpnb.lexer;
+
+import org.netbeans.api.lexer.TokenId;
+
+public class SilverstripeTokenId implements TokenId {
+
+    @Override
+    public String name() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int ordinal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String primaryCategory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
